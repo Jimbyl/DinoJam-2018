b117b81cea9ca1008f30368d7f8f64de07b1394a
Myserval <myserval@gmail.com>
2018-07-25 22:15:34 -0600

    Added urchin Monster

---
+++ KinematicBody2D.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+var normalVec = Vector2(0,-1)
+var speed = 75
+var knockback = 100
+var health = 4
+func _physics_process(delta):
+	var player = get_tree().get_nodes_in_group("SwimmingAI")[0]
+	var monster = get_tree().get_nodes_in_group("SwimmingAI")[1]
+	var angle = monster.get_angle_to(player.position)
+	motion = polar2cartesian(speed, angle)
+	if(global_position.distance_to(player.position) < 60): #attack player
+		player.motion += polar2cartesian(knockback, angle)
+		player.health -= 1
+	get_node("HBoxContainer/Health").set_text(str(health))
