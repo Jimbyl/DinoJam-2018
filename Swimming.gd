8e79e23def14553ccb12f4675787bb2e6f7abfa0
Myserval <myserval@gmail.com>
2018-07-26 13:08:23 -0600

    Added support for more maps

--- Swimming.gd
+++ Swimming.gd
@@ -4,38 +4,37 @@ var money_total = 0
 var money_collected = 0
 
 func _ready():
-	randomize()
-	var map = randi() % 5 + 1   #comment this out to load default map
-	if map == 1:  
-		var scene = load("res://Map scenes/Map1.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
-	if map == 2:
-		var scene = load("res://Map scenes/Map2.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
-	if map == 3:
-		var scene = load("res://Map scenes/Map3.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
-	if map == 4:
-		var scene = load("res://Map scenes/Map4.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
-	if map == 5:
-		var scene = load("res://Map scenes/Map5.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
-	else:
-		var scene = load("res://Map scenes/Maptest.tscn")
-		var scene_instance = scene.instance()
-		scene_instance.set_name("Map")
-		self.add_child(scene_instance)
+	var map = 0
+	randomize ()
+	map = randi() % 5 + 1   #comment this out to load default map
+	if map != 0 :
+		var testmap = get_node("Map")
+		remove_child(testmap)
+		if map == 1:  
+			var scene = load("res://Map scenes/Map1.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Map")
+			self.add_child(scene_instance)
+		if map == 2:
+			var scene = load("res://Map scenes/Map2.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Map")
+			self.add_child(scene_instance)
+		if map == 3:
+			var scene = load("res://Map scenes/Map3.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Map")
+			self.add_child(scene_instance)
+		if map == 4:
+			var scene = load("res://Map scenes/Map4.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Map")
+			self.add_child(scene_instance)
+		if map == 5:
+			var scene = load("res://Map scenes/Map5.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Map")
+			self.add_child(scene_instance)
 	pass
 
 func _on_Button_pressed():
