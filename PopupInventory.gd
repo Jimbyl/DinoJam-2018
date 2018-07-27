3c56b9a3be981c5d8d3a82c31eaf1c99b8b82509
Myserval <myserval@gmail.com>
2018-07-26 23:11:25 -0600

    added control for opening and closing inventory

---
+++ PopupInventory.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var inventory_open =0
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_focus_next"):
+		if inventory_open ==0:
+			print("inventoryopen")
+			var scene = load("res://PlayerInventory.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("Inventory")
+			self.add_child(scene_instance)
+			inventory_open = 1
+		elif inventory_open ==1:
+			print ("inventoryclose")	
+			var inventory = get_node("Inventory")
+			remove_child(inventory)
+			inventory_open = 0
