428571dce67408c00fb48390739dbc21fe51a713
BarkleysPowerDunk <41239793+BarkleysPowerDunk@users.noreply.github.com>
2018-07-28 00:13:41 -0600

    Added Moby Dick functionality

--- Bullet.gd
+++ Bullet.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 const BULLET_SPEED = 400
 
+var damage = 1
 
 onready var speed_x = 1
 onready var speed_y = 0
@@ -11,6 +12,10 @@ func _ready():
 	var position = get_global_position()
 	if position.y > 0:
 		get_node("Particles2D").set_emitting(true)
+		
+	if Global_Player.inventory_checkItem("10") == true:
+		for toCompare in get_tree().get_nodes_in_group("Fish"):
+				damage = 2
 
 func _process(delta):
 	var motion = Vector2(speed_x, speed_y) * BULLET_SPEED
@@ -27,4 +32,7 @@ func _on_Area2D_body_entered(body):
 	for toCompare in get_tree().get_nodes_in_group("SwimmingAI"):
 		if body == toCompare:
 			body.health -=1
-	queue_free()
+		if Global_Player.inventory_checkItem("10") == true:
+			for toCompare in get_tree().get_nodes_in_group("SwimmingAI"):
+				if body == toCompare:
+					body.health -=1
