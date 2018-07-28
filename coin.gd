32f391d8fbbada279e5bc0ee704836dffe27aa07
Will <willjnnr57@gmail.com>
2018-07-28 14:32:43 -0600

    Coins work now

--- coin.gd
+++ coin.gd
@@ -1,23 +1,18 @@
 extends Node2D
 
 export var value = 1
-
-
-
+onready var swimmingScene = get_tree().get_nodes_in_group("SwimmingAI")[0].get_parent()
 func _ready():
-	
-	if get_owner() != null:
-		get_owner().money_total += value
+	swimmingScene.money_total += value
 	get_node("AnimationPlayer").play("coin")
 
 
 func _collect_money( body ):
 	if get_node("AnimationPlayer").get_current_animation() != "collect":
-		if get_owner() != null:
-			get_owner().money_collected += value
-			get_owner().get_node("Player/Camera2D/gui/HBoxContainer/money_count").set_text(str(get_owner().money_collected))
-			print(get_owner().money_collected)
-			
+		
+		swimmingScene.money_collected += value
+		swimmingScene.get_node("Player/Camera2D/gui/HBoxContainer/money_count").set_text(str(swimmingScene.money_collected))
+		print(swimmingScene.money_collected)
 		get_node("AnimationPlayer").play("collect")
 		queue_free()
 
