61747b01ca8fe4fb56a5a6fd6299481ff6a04498
Will <willjnnr57@gmail.com>
2018-07-28 23:00:52 -0600

    Coins go in inventory now

--- coin.gd
+++ coin.gd
@@ -3,16 +3,15 @@ extends Node2D
 export var value = 1
 onready var swimmingScene = get_tree().get_nodes_in_group("SwimmingAI")[0].get_parent()
 func _ready():
-	swimmingScene.money_total += value
 	get_node("AnimationPlayer").play("coin")
+	swimmingScene.get_node("Player/Camera2D/gui/HBoxContainer/money_count").set_text(str(Global_Player.inventory_checkItemAmount("3")))
 
 
 func _collect_money( body ):
 	if get_node("AnimationPlayer").get_current_animation() != "collect":
 		
-		swimmingScene.money_collected += value
-		swimmingScene.get_node("Player/Camera2D/gui/HBoxContainer/money_count").set_text(str(swimmingScene.money_collected))
-		print(swimmingScene.money_collected)
+		Global_Player.inventory_addItem("3")
+		swimmingScene.get_node("Player/Camera2D/gui/HBoxContainer/money_count").set_text(str(Global_Player.inventory_checkItemAmount("3")))
 		get_node("AnimationPlayer").play("collect")
 		queue_free()
 
