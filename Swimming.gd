a7d1d98e74076f671f2b17f7ed2cb71adeb9d958
Will <willjnnr57@gmail.com>
2018-07-28 20:12:47 -0600

    Made rocks and guns and maps a little better

--- Swimming.gd
+++ Swimming.gd
@@ -10,41 +10,10 @@ func _ready():
 	if map != 0 :
 		var testmap = get_node("Map")
 		remove_child(testmap)
-		if map == 1:  
-			var scene = load("res://Map scenes/Map1.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 2:
-			var scene = load("res://Map scenes/Map2.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 3:
-			var scene = load("res://Map scenes/Map3.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 4:
-			var scene = load("res://Map scenes/Map4.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 5:
-			var scene = load("res://Map scenes/Map5.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 6:
-			var scene = load("res://Map scenes/Map6.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
-		if map == 7:
-			var scene = load("res://Map scenes/Map7.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("Map")
-			self.add_child(scene_instance)
+		var scene = load("res://Map scenes/Map"+String(map)+".tscn")
+		var scene_instance = scene.instance()
+		scene_instance.set_name("Map")
+		self.add_child(scene_instance)
 	pass
 
 func _on_Button_pressed():
